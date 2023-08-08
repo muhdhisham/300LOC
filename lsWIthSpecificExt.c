8462bc2ed9c8643a3eeec2c2ba56d725d7ad179d
Muhammed Hisham V <muhdhisham034@gmail.com>
2023-08-08 09:26:35 +0530

    Add files via upload

---
+++ lsWIthSpecificExt.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+void listFilesRecursively(const char *basePath, const char *targetExtension) {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat fileStat;
+    
+    if (!(dir = opendir(basePath))) {
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        char path[1024];
+        snprintf(path, sizeof(path), "%s/%s", basePath, entry->d_name);
+
+        if (stat(path, &fileStat) == -1) {
+            continue;
+        }
+
+        if (S_ISDIR(fileStat.st_mode)) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+            listFilesRecursively(path, targetExtension);
+        } else {
+            // Check if the file has the desired extension
+            const char *fileExtension = strrchr(entry->d_name, '.');
+            if (fileExtension && strcmp(fileExtension, targetExtension) == 0) {
+                printf("%s\n", path);
+            }
+        }
+    }
+    closedir(dir);
+}
+
+int main() {
+    const char *directoryPath = "."; // Change this to your target directory
+    const char *targetExtension = ".txt"; // Change this to the desired extension
+    
+    printf("Files with extension %s in directory %s:\n", targetExtension, directoryPath);
+    listFilesRecursively(directoryPath, targetExtension);
+
+    return 0;
+}
---
+++ move.c
@@ -0,0 +1,54 @@
+// C program that demonstrates
+// the use of remove() function
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+   char ch, source_file[20], target_file[20];
+   FILE *source, *target;
+ 
+   printf("Enter name of file to move\n");
+   gets(source_file);
+ 
+   source = fopen(source_file, "r");
+ 
+   if( source == NULL )
+   {
+      printf("Press any key to exit...\n");
+      exit(EXIT_FAILURE);
+   }
+ 
+   printf("Enter name of target file\n");
+   gets(target_file);
+ 
+   target = fopen(target_file, "w");
+ 
+   if( target == NULL )
+   {
+      fclose(source);
+      printf("Press any key to exit...\n");
+      exit(EXIT_FAILURE);
+   }
+ 
+   while( ( ch = fgetc(source) ) != EOF )
+      fputc(ch, target);
+ 
+   printf("File copied successfully.\n");
+fclose(source);
+int ret = remove(source_file);
+
+   if(ret == 0) {
+      printf("File deleted successfully");
+   } else {
+      printf("Error: unable to delete the file");
+   }
+ 
+   
+   fclose(target);
+ 
+   return 0;
+}
+
+
+
